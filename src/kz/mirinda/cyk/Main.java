64ac1fc061cebe9a589a306642a37cd6c8d75fd0
Dmitriy Mirin <merlin21demon@mail.ru>
2012-06-05 21:46:23 +0600

    Idea Hello world!

---
+++ src/kz/mirinda/cyk/Main.java
@@ -0,0 +1,12 @@
+package kz.mirinda.cyk;
+
+/**
+ * new class
+ *
+ * @author mirinda
+ */
+public class Main {
+     public  static void  main(String[] args){
+         System.out.println("Hello  World!");
+     }
+}
